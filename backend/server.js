7ec64dbd1959579908a05512bc69df1685d0968a
Rupak swar <116864759+ai-rupak@users.noreply.github.com>
2025-07-27 20:12:35 +0530

    🚀

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use(cors({
-  origin: 'https://mediqueue-seven.vercel.app/',
+  origin: 'https://mediqueue-seven.vercel.app',
   credentials: true,
 }));
 
